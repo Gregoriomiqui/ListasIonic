5592af5abf7c4e453a41deda0685aeb0dbe63cb6
gregoriomiqui <gregoriomiqui@gmail.com>
2018-03-21 16:50:44 -0400

    carga de datos en la pagina contacto

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,36 +16,36 @@ export class HomePage {
 
   //Objeto de contactos
   contactos=[
-  {
-    nombre:'Andres Alvarez',
-    telefono:'04148305033',
-    correo:'gregoriomiqui@gmail.com',
-    imagen:'avatar-andres.png'
-  },
-  {
-    nombre:'Ana Prado',
-    telefono:'04148575339',
-    correo:'anaprado@gmail.com',
-    imagen:'avatar-ana.png'
-  },
-  {
-    nombre:'Carlos Ruiz',
-    telefono:'04145033456',
-    correo:'cruiz@gmail.com',
-    imagen:'avatar-carlos.png'
-  },
-  { 
-    nombre:'Lucia Fermin',
-    telefono:'04165891471',
-    correo:'luciafermin@gmail.com',
-    imagen:'avatar-lucia.png'
-  },
-  { 
-    nombre:'Miguel Salazar',
-    telefono:'04147856123',
-    correo:'msalazar@gmail.com',
-    imagen:'avatar-miguel'
-  }
+    {
+      nombre:'Andres Alvarez',
+      telefono:'04148305033',
+      correo:'gregoriomiqui@gmail.com',
+      imagen:'avatar-andres.png'
+    },
+    {
+      nombre:'Ana Prado',
+      telefono:'04148575339',
+      correo:'anaprado@gmail.com',
+      imagen:'avatar-ana.png'
+    },
+    {
+      nombre:'Carlos Ruiz',
+      telefono:'04145033456',
+      correo:'cruiz@gmail.com',
+      imagen:'avatar-carlos.png'
+    },
+    { 
+      nombre:'Lucia Fermin',
+      telefono:'04165891471',
+      correo:'luciafermin@gmail.com',
+      imagen:'avatar-lucia.png'
+    },
+    { 
+      nombre:'Miguel Salazar',
+      telefono:'04147856123',
+      correo:'msalazar@gmail.com',
+      imagen:'avatar-miguel.png'
+    }
   ]
 
 
